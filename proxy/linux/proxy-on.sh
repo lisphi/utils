2e3469f2b4c517588b8b465cacf20868b5a24aa7
Benja <lisphi@qq.com>
2026-01-11 12:02:58 +0800

    proxy linux

---
+++ proxy/linux/proxy-on.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set proxy
+export http_proxy="http://127.0.0.1:7897"
+export https_proxy="http://127.0.0.1:7897"
+export no_proxy="localhost,127.0.0.1,::1,.ctripcorp.com,.ctrip.com,.trip.com,.c-ctrip.com,.tripcdn.com,.tripqate.com,.tripws.com,.baidu.com,.feishu.cn,.larkenterprise.com"
+
+# Check proxy
+ip=$(curl -s https://api.ipify.org)
+if [ -n "$ip" ] && [[ "$ip" =~ ^[0-9.]+$ ]]; then
+    echo -e "IP Address: $ip"
+    # Get geolocation info
+    geo=$(curl -s "http://ip-api.com/json/$ip")
+    country=$(echo $geo | jq -r '.country')
+    region=$(echo $geo | jq -r '.regionName')
+    city=$(echo $geo | jq -r '.city')
+    echo -e "Location: $country $region $city"
+else
+    echo -e "Unable to get the outgoing IP, the network may be disconnected or blocked by a firewall."
+fi
+
---
+++ proxy/linux/proxy-status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check proxy
+ip=$(curl -s https://api.ipify.org)
+if [ -n "$ip" ] && [[ "$ip" =~ ^[0-9.]+$ ]]; then
+    echo -e "IP Address: $ip"
+    # Get geolocation info
+    geo=$(curl -s "http://ip-api.com/json/$ip")
+    country=$(echo $geo | jq -r '.country')
+    region=$(echo $geo | jq -r '.regionName')
+    city=$(echo $geo | jq -r '.city')
+    echo -e "Location: $country $region $city"
+else
+    echo -e "Unable to get the outgoing IP, the network may be disconnected or blocked by a firewall."
+fi
+
